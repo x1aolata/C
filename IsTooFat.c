aefad3bc6bfa43156d5722a99870c288d4cf39e9
x1aolata <45487873+x1aolata@users.noreply.github.com>
2018-12-16 21:55:09 +0800

    learning

---
+++ IsTooFat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main()
+{
+    system("color 02");
+    int hight;
+    double weight;
+    scanf("%d",&hight);
+    weight=(double)(hight-100)*1.8;
+    printf("%.1lf",weight);
+
+
+
+
+
+
+    return 0;
+}
