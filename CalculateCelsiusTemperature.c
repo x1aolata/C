2803d29baf54fb05bf88ba9a643a6def6a88eb72
x1aolata <45487873+x1aolata@users.noreply.github.com>
2019-01-21 12:44:30 +0800

    learn

---
+++ CalculateCelsiusTemperature.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main()
+{
+    system("color 02");
+    int a,b;
+    scanf("%d",&a);
+    b=5*(a-32)/9;
+    printf("Celsius = %d",b);
+
+
+
+
+    return 0;
+}
